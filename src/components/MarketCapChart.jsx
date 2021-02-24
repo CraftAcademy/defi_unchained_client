49add7e426bcbae0a8bbe50b2147b955fc4df677
FoxyTony <mirantony@icloud.com>
2021-02-24 11:29:43 +0100

    Last market cap is properly shown

--- src/components/MarketCapChart.jsx
+++ src/components/MarketCapChart.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 import { getMarketCapData } from '../modules/DataCenter'
 import axios from 'axios'
 
 class MarketCapChart extends React.Component {
   state = {
     market_data: []
+
   }
   componentDidMount = async () => {
     let response = await getMarketCapData()
-    this.setState({market_data: response.market_data})
+    
+    this.setState({ market_data: response.market_data })
+    debugger
   }
 
   render() {
@@ -18,12 +22,14 @@ class MarketCapChart extends React.Component {
       )
     })
     return (
-      <div>
-        HELLO LUCAS
-         {list}
-      </div>
+      <Container>
+{this.state.market_data[1] && <h1>{this.state.market_data[this.state.market_data.length - 1].market_cap}</h1>} 
+        </Container>
+
+
     )
   }
 }
 
 export default MarketCapChart;
+
